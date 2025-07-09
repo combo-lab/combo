248ad699a30a2c9003951f7f9cace88a95e7a37c
Zeke Dou <zeke@zekedou.live>
2025-07-09 20:57:00 +0800

    refactor: improve .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -63,11 +63,13 @@ locals_without_parens = [
   # Phoenix.ConnTest
   assert_error_sent: 2,
 
+  # Phoenix.Template
+  embed_templates: 1,
+  embed_templates: 2,
+
   # Phoenix.Live{Dashboard,View}
   attr: 2,
   attr: 3,
-  embed_templates: 1,
-  embed_templates: 2,
   live: 2,
   live: 3,
   live: 4,
