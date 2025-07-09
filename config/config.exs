9cdeb5b6d928815c78b244d2e8c3e1a9719c0bee
Zeke Dou <zeke@zekedou.live>
2025-07-09 21:34:27 +0800

    test: make Phoenix.Template tests work again

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,7 @@ config :logger, :console,
 config :phoenix,
   json_library: Jason,
   stacktrace_depth: 20,
-  trim_on_html_eex_engine: false
+  trim_on_html_eex_engine: true
 
 if Mix.env() == :dev do
   esbuild = fn args ->
