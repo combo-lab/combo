d219ec1d714d2e5dc6ff0a3038390648fb13a61f
Zeke Dou <zeke@zekedou.live>
2025-08-05 12:05:20 +0800

    test: merge tests

--- test/phoenix/html/components_test.exs
+++ test/phoenix/html/components_test.exs
@@ -89,6 +89,61 @@ defmodule Phoenix.HTML.ComponentsTest do
     end
   end
 
+  describe "to_form/2" do
+    test "with a map" do
+      form = to_form(%{})
+      assert form.name == nil
+      assert form.id == nil
+
+      form = to_form(%{}, as: :foo)
+      assert form.name == "foo"
+      assert form.id == "foo"
+
+      form = to_form(%{}, as: :foo, id: "bar")
+      assert form.name == "foo"
+      assert form.id == "bar"
+
+      form = to_form(%{}, custom: "attr")
+      assert form.options == [custom: "attr"]
+
+      form = to_form(%{}, errors: [name: "can't be blank"])
+      assert form.errors == [name: "can't be blank"]
+    end
+
+    test "with a form" do
+      base = to_form(%{}, as: "name", id: "id")
+      assert to_form(base, []) == base
+
+      form = to_form(base, as: :foo)
+      assert form.name == "foo"
+      assert form.id == "foo"
+
+      form = to_form(base, id: "bar")
+      assert form.name == "name"
+      assert form.id == "bar"
+
+      form = to_form(base, as: :foo, id: "bar")
+      assert form.name == "foo"
+      assert form.id == "bar"
+
+      form = to_form(base, as: nil, id: nil)
+      assert form.name == nil
+      assert form.id == nil
+
+      form = to_form(base, custom: "attr")
+      assert form.options[:custom] == "attr"
+
+      form = to_form(base, errors: [name: "can't be blank"])
+      assert form.errors == [name: "can't be blank"]
+
+      form = to_form(base, action: :validate)
+      assert form.action == :validate
+
+      form = to_form(%{base | action: :validate})
+      assert form.action == :validate
+    end
+  end
+
   describe "form/1" do
     test "renders form with prebuilt form" do
       assigns = %{form: to_form(%{})}
