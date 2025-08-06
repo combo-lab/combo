127913c8e446864c8a120fef903c5d79bcb82fe1
Zeke Dou <zeke@zekedou.live>
2025-08-06 12:28:26 +0800

    upstream(phoenix_live_view): patch 6c39e4802198b43ea8f0f5c5c1ca7fbbab7dbfc8
    - https://github.com/phoenixframework/phoenix_live_view/commit/6c39e4802198b43ea8f0f5c5c1ca7fbbab7dbfc8

--- test/phoenix/html/formatter_test.exs
+++ test/phoenix/html/formatter_test.exs
@@ -68,9 +68,7 @@ defmodule Combo.HTML.FormatterTest do
     expected = """
     <section>
       <h1>
-        <b>
-          <%= @user.name %>
-        </b>
+        <b><%= @user.name %></b>
       </h1>
     </section>
     """
@@ -86,8 +84,7 @@ defmodule Combo.HTML.FormatterTest do
     expected = """
     <section>
       <h1>
-        <b class="there are several classes">
-        </b>
+        <b class="there are several classes"></b>
       </h1>
     </section>
     """
@@ -340,11 +337,9 @@ defmodule Combo.HTML.FormatterTest do
 
     expected = """
     <%= if true do %>
-      <p>do something</p>
-      <p>more stuff</p>
+      <p>do something</p><p>more stuff</p>
     <% else %>
-      <p>do something else</p>
-      <p>more stuff</p>
+      <p>do something else</p><p>more stuff</p>
     <% end %>
     """
 
@@ -482,8 +477,7 @@ defmodule Combo.HTML.FormatterTest do
     <section>
       <%= live_redirect to: "url", id: "link", role: "button" do %>
         <div>
-          <p>content 1</p>
-          <p>content 2</p>
+          <p>content 1</p><p>content 2</p>
         </div>
       <% end %>
       <p><%= @user.name %></p>
@@ -568,9 +562,7 @@ defmodule Combo.HTML.FormatterTest do
       """
       <p>
         first
-        <span>
-          name
-        </span>
+        <span>name</span>
         second
       </p>
       """,
@@ -608,13 +600,9 @@ defmodule Combo.HTML.FormatterTest do
       <span><%= @user_a %></span> X <span><%= @user_b %></span>
       """,
       """
-      <span>
-        <%= @user_a %>
-      </span>
+      <span><%= @user_a %></span>
       X
-      <span>
-        <%= @user_b %>
-      </span>
+      <span><%= @user_b %></span>
       """,
       line_length: 5
     )
@@ -1738,9 +1726,32 @@ defmodule Combo.HTML.FormatterTest do
       line_length: 20
     )
 
-    assert_formatter_doesnt_change(
+    assert_formatter_output(
       """
       <b>foo</b><i><span><div>bar</div></span></i><span>baz</span>
+      """,
+      """
+      <b>foo</b><i><span><div>
+        bar
+      </div></span></i><span>baz</span>
+      """,
+      line_length: 20
+    )
+  end
+
+  test "does not add space between elements without space" do
+    assert_formatter_doesnt_change(
+      """
+      <span>foo</span><span>bar</span>
+      <span>foo</span><.foo_bar_baz />
+      <.foo_bar_baz /><span>bar</span>
+      <div>foo</div><div>
+        bar
+      </div>
+      <div>foo</div><.foo_bar_baz />
+      <.foo_bar_baz /><div>
+        bar
+      </div>
       """,
       line_length: 20
     )
@@ -2034,9 +2045,7 @@ defmodule Combo.HTML.FormatterTest do
       """
       <p>
         foo
-        <strong class="foo bar baz">
-          <%= some_function() %>
-        </strong>
+        <strong class="foo bar baz"><%= some_function() %></strong>
         baz
       </p>
       """,
@@ -2390,8 +2399,7 @@ defmodule Combo.HTML.FormatterTest do
     <section>
       <%= live_redirect to: "url", id: "link", role: "button" do %>
         <div>
-          <p>content 1</p>
-          <p>content 2</p>
+          <p>content 1</p><p>content 2</p>
         </div>
       <% end %>
       <p>{@user.name}</p>
@@ -2443,9 +2451,7 @@ defmodule Combo.HTML.FormatterTest do
     expected = """
     <section>
       <h1>
-        <b class="there are several classes">
-          {@user.name}
-        </b>
+        <b class="there are several classes">{@user.name}</b>
       </h1>
     </section>
     """
