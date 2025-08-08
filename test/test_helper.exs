c82a2c59e013acf5570778af38d9a48034f7d1bd
Zeke Dou <zeke@zekedou.live>
2025-08-08 23:01:00 +0800

    test: add a mechanism for setting up environments for tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,33 @@
-Application.put_env(:phoenix_live_view, :debug_heex_annotations, true)
-Code.require_file("support/debug_annotations.exs", __DIR__)
-Application.put_env(:phoenix_live_view, :debug_heex_annotations, false)
+# A mechanism for setting up environments for tests, designed to help
+# keep test code and environment setup code as closer as possible.
+#
+# It:
+#
+#   1. find directories ends with `_test`
+#   2. run `*_test/setup.exs`
+#
+# It should be used for the cases that setup/_ and setup_all/_ can't
+# handle. In general cases, setup/_ and setup_all/_ should be used.
+#
+# How to use it?
+# Suppose that we have a test - `test/combo/complex_logic_test.exs`.
+#
+#   1. create the directory - `test/combo/complex_logic_test`.
+#   2. add a file `setup.exs` into above directory.
+#   3. write setups in above file.
+#
+setup_dirs =
+  Path.join(__DIR__, "**/*_test")
+  |> Path.wildcard()
+  |> Enum.filter(&File.dir?/1)
+
+for dir <- setup_dirs do
+  setup_file = Path.join(dir, "setup.exs")
+  if File.exists?(setup_file) do
+    IO.puts("> eval test/#{Path.relative_to(setup_file, __DIR__)}")
+    Code.eval_file(setup_file)
+  end
+end
 
 Code.require_file("support/router_helper.exs", __DIR__)
 
