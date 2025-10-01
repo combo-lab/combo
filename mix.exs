6d0e5733aea34ef3b53de887257ff0d722813f18
Zeke Dou <zeke@zekedou.live>
2025-10-01 16:31:37 +0800

    release: bump v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Combo.MixProject do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.3.0"
   @description "A web framework, that combines the good parts of modern web development."
   @elixir_requirement "~> 1.18"
   @source_url "https://github.com/combo-lab/combo"
