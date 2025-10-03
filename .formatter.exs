f8e12de52ff2d5765d0e62f19152c5629105d63f
Zeke Dou <zeke@zekedou.live>
2025-10-03 21:19:10 +0800

    chore: prepare for formatting code

--- .formatter.exs
+++ .formatter.exs
@@ -78,5 +78,6 @@ locals_without_parens = [
 [
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens],
-  plugins: [Combo.HTML.Formatter]
+  plugins: [Combo.HTML.Formatter],
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
