e57f36b7c1b81088763f46845f02caf0912b87ae
Zeke Dou <zeke@zekedou.live>
2025-11-13 11:22:40 +0800

    feat(router): rollback console formatter

--- test/combo/router/console_formatter_test.exs
+++ test/combo/router/console_formatter_test.exs
@@ -10,7 +10,7 @@ defmodule Combo.Router.ConsoleFormatterTest do
   alias Combo.Router.ConsoleFormatter
 
   defmodule RouterTestSingleRoutes do
-    use Support.Router
+    use Combo.Router
 
     get "/", RouteFormatter.PageController, :index, as: :page
     post "/images", RouteFormatter.ImageController, :upload, as: :upload_image
@@ -27,32 +27,32 @@ defmodule Combo.Router.ConsoleFormatterTest do
 
   test "format multiple routes" do
     assert draw(RouterTestSingleRoutes) == """
-           GET     /        RouteFormatter.PageController :index
-           POST    /images  RouteFormatter.ImageController :upload
-           DELETE  /images  RouteFormatter.ImageController :delete
+                   page_path  GET     /        RouteFormatter.PageController :index
+           upload_image_path  POST    /images  RouteFormatter.ImageController :upload
+           remove_image_path  DELETE  /images  RouteFormatter.ImageController :delete
            """
   end
 
   defmodule RouterTestResources do
-    use Support.Router
+    use Combo.Router
     resources "/images", RouteFormatter.ImageController
   end
 
   test "format resource routes" do
     assert draw(RouterTestResources) == """
-           GET     /images           RouteFormatter.ImageController :index
-           GET     /images/:id/edit  RouteFormatter.ImageController :edit
-           GET     /images/new       RouteFormatter.ImageController :new
-           GET     /images/:id       RouteFormatter.ImageController :show
-           POST    /images           RouteFormatter.ImageController :create
-           PATCH   /images/:id       RouteFormatter.ImageController :update
-           PUT     /images/:id       RouteFormatter.ImageController :update
-           DELETE  /images/:id       RouteFormatter.ImageController :delete
+           image_path  GET     /images           RouteFormatter.ImageController :index
+           image_path  GET     /images/:id/edit  RouteFormatter.ImageController :edit
+           image_path  GET     /images/new       RouteFormatter.ImageController :new
+           image_path  GET     /images/:id       RouteFormatter.ImageController :show
+           image_path  POST    /images           RouteFormatter.ImageController :create
+           image_path  PATCH   /images/:id       RouteFormatter.ImageController :update
+                       PUT     /images/:id       RouteFormatter.ImageController :update
+           image_path  DELETE  /images/:id       RouteFormatter.ImageController :delete
            """
   end
 
   defmodule RouterTestResource do
-    use Support.Router
+    use Combo.Router
     resources "/image", RouteFormatter.ImageController, singleton: true
     forward "/admin", RouteFormatter.PageController, [], as: :admin
     forward "/f1", RouteFormatter.ImageController
@@ -60,37 +60,69 @@ defmodule Combo.Router.ConsoleFormatterTest do
 
   test "format single resource routes" do
     assert draw(RouterTestResource) == """
-           GET     /image/edit  RouteFormatter.ImageController :edit
-           GET     /image/new   RouteFormatter.ImageController :new
-           GET     /image       RouteFormatter.ImageController :show
-           POST    /image       RouteFormatter.ImageController :create
-           PATCH   /image       RouteFormatter.ImageController :update
-           PUT     /image       RouteFormatter.ImageController :update
-           DELETE  /image       RouteFormatter.ImageController :delete
-           *       /admin       RouteFormatter.PageController []
-           *       /f1          RouteFormatter.ImageController []
+           image_path  GET     /image/edit  RouteFormatter.ImageController :edit
+           image_path  GET     /image/new   RouteFormatter.ImageController :new
+           image_path  GET     /image       RouteFormatter.ImageController :show
+           image_path  POST    /image       RouteFormatter.ImageController :create
+           image_path  PATCH   /image       RouteFormatter.ImageController :update
+                       PUT     /image       RouteFormatter.ImageController :update
+           image_path  DELETE  /image       RouteFormatter.ImageController :delete
+                       *       /admin       RouteFormatter.PageController []
+                       *       /f1          RouteFormatter.ImageController []
            """
   end
 
   describe "endpoint sockets" do
     test "format with sockets" do
       assert draw(RouterTestSingleRoutes, FormatterEndpoint) == """
-             GET     /                  RouteFormatter.PageController :index
-             POST    /images            RouteFormatter.ImageController :upload
-             DELETE  /images            RouteFormatter.ImageController :delete
-             WS      /socket/websocket  TestSocket
-             GET     /socket/longpoll   TestSocket
-             POST    /socket/longpoll   TestSocket
+                     page_path  GET     /                  RouteFormatter.PageController :index
+             upload_image_path  POST    /images            RouteFormatter.ImageController :upload
+             remove_image_path  DELETE  /images            RouteFormatter.ImageController :delete
+                                WS      /socket/websocket  TestSocket
+                                GET     /socket/longpoll   TestSocket
+                                POST    /socket/longpoll   TestSocket
              """
     end
 
     test "format without sockets" do
       assert draw(RouterTestSingleRoutes, __MODULE__) == """
-             GET     /        RouteFormatter.PageController :index
-             POST    /images  RouteFormatter.ImageController :upload
-             DELETE  /images  RouteFormatter.ImageController :delete
+                     page_path  GET     /        RouteFormatter.PageController :index
+             upload_image_path  POST    /images  RouteFormatter.ImageController :upload
+             remove_image_path  DELETE  /images  RouteFormatter.ImageController :delete
              """
     end
+  end
+
+  defmodule HelpersFalseRouter do
+    use Combo.Router, helpers: false
+    resources "/image", RouteFormatter.ImageController
+  end
+
+  test "helpers: false" do
+    assert draw(HelpersFalseRouter) == """
+             GET     /image           RouteFormatter.ImageController :index
+             GET     /image/:id/edit  RouteFormatter.ImageController :edit
+             GET     /image/new       RouteFormatter.ImageController :new
+             GET     /image/:id       RouteFormatter.ImageController :show
+             POST    /image           RouteFormatter.ImageController :create
+             PATCH   /image/:id       RouteFormatter.ImageController :update
+             PUT     /image/:id       RouteFormatter.ImageController :update
+             DELETE  /image/:id       RouteFormatter.ImageController :delete
+           """
+
+    assert draw(HelpersFalseRouter, FormatterEndpoint) == """
+             GET     /image             RouteFormatter.ImageController :index
+             GET     /image/:id/edit    RouteFormatter.ImageController :edit
+             GET     /image/new         RouteFormatter.ImageController :new
+             GET     /image/:id         RouteFormatter.ImageController :show
+             POST    /image             RouteFormatter.ImageController :create
+             PATCH   /image/:id         RouteFormatter.ImageController :update
+             PUT     /image/:id         RouteFormatter.ImageController :update
+             DELETE  /image/:id         RouteFormatter.ImageController :delete
+             WS      /socket/websocket  TestSocket
+             GET     /socket/longpoll   TestSocket
+             POST    /socket/longpoll   TestSocket
+           """
   end
 
   defp draw(router, endpoint \\ nil) do
