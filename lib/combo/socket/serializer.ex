ffcf8308a57c495361acc94a452a9fe16e5d9b2b
Zeke Dou <zeke@zekedou.live>
2026-08-08 19:56:29 +0800

    socket: polish serializer

--- lib/combo/socket/serializer.ex
+++ lib/combo/socket/serializer.ex
@@ -8,22 +8,23 @@ defmodule Combo.Socket.Serializer do
   Custom serializers may be configured in the socket.
   """
 
+  @type opcode :: :text | :binary
+
   @doc """
-  Encodes a `Combo.Socket.Broadcast` struct to fastlane format.
+  Encodes a `%Combo.Socket.Broadcast{}` to fastlane format.
   """
   @callback fastlane!(Combo.Socket.Broadcast.t()) ::
-              {:socket_push, :text, iodata()}
-              | {:socket_push, :binary, iodata()}
+              {:socket_push, opcode(), iodata()}
 
   @doc """
-  Encodes `Combo.Socket.Message` and `Combo.Socket.Reply` structs to push format.
+  Encodes `%Combo.Socket.Message{}` and `%Combo.Socket.Reply{}` to push format.
   """
   @callback encode!(Combo.Socket.Message.t() | Combo.Socket.Reply.t()) ::
-              {:socket_push, :text, iodata()}
-              | {:socket_push, :binary, iodata()}
+              {:socket_push, opcode(), iodata()}
 
   @doc """
-  Decodes iodata into `Combo.Socket.Message` struct.
+  Decodes iodata into `%Combo.Socket.Message{}`.
   """
-  @callback decode!(iodata, options :: keyword()) :: Combo.Socket.Message.t()
+  @callback decode!(iodata, options :: keyword()) ::
+              Combo.Socket.Message.t()
 end
