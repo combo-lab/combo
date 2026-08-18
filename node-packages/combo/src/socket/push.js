0f15279ea4ede80958e3889c21668bb5f5367f1f
Zeke Dou <zeke@zekedou.live>
2026-08-18 22:05:13 +0800

    [upstream] socket client: release the stale reply binding of a buffered push
    - https://github.com/phoenixframework/phoenix/commit/4c118407d0cb395b1bf4d5f257fbd13e674df427

--- node-packages/combo/src/socket/push.js
+++ node-packages/combo/src/socket/push.js
@@ -106,6 +106,7 @@ export default class Push {
     if (this.timeoutTimer) {
       this.cancelTimeout()
     }
+    this.cancelRefEvent()
     this.ref = this.channel.socket.makeRef()
     this.refEvent = this.channel.replyEventName(this.ref)
 
